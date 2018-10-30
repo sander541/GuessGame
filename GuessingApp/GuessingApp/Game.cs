81c9b08e0afedadf595abee3a2160a1817e4b33d
sander541 <sandersven1@gmail.com>
2018-10-30 15:02:53 +0200

    refactoring

--- GuessingApp/GuessingApp/Game.cs
+++ GuessingApp/GuessingApp/Game.cs
@@ -11,23 +11,23 @@ namespace GuessingApp
         }
         public static int GenerateRandomNumber()
         {
-            Random random = new Random();
-            int Number = random.Next(1, 300);
+            Random RandomNumber = new Random();
+            int AnswerNumber = RandomNumber.Next(1, 300);
 
-            return Number;
+            return AnswerNumber;
         }
 
         public static int GetUserNumber()
         {
-            Console.Write("Please select a number : ");
-            bool isNumber = Int32.TryParse(Console.ReadLine(), out int GuessedNumber);
+            Console.Write("Please select a number between 1 - 300 : ");
+            Int32.TryParse(Console.ReadLine(), out int GuessedNumber);
 
             return IsNumberCorrect(GuessedNumber);
         }
 
         public static int IsNumberCorrect(int GuessedNumber)
         {
-            if (IsValidRangeNumber(GuessedNumber))
+            if (GuessedNumber <= 300 && GuessedNumber >= 1)
             {
                 return GuessedNumber;
             }
@@ -38,40 +38,32 @@ namespace GuessingApp
             }
         }
 
-        public static bool IsValidRangeNumber(int selectedNumber)
-        {
-            if (selectedNumber <= 300 && selectedNumber >= 1)
-                return true;
-            else
-                return false;
-        }
-
         public static void StartGame()
         {
-            int CorrectNumber = GenerateRandomNumber();
+            int CorrectAnswer = GenerateRandomNumber();
             int GuessCount = 0;
             bool CorrectGuess = false;
 
             while (!CorrectGuess)
             {
                 int GuessedNumber = GetUserNumber();
-
                 GuessCount++;
-                if (GuessedNumber > CorrectNumber)
+
+                if (GuessedNumber > CorrectAnswer)
                 {
                     Console.WriteLine("Your guess, '{0}', was higher than correct answer -- move: '{1}'", GuessedNumber, GuessCount);
                 }
-                if (GuessedNumber < CorrectNumber)
+                if (GuessedNumber < CorrectAnswer)
                 {
                     Console.WriteLine("Your guess, '{0}', was lower than correct answer -- move: '{1}'", GuessedNumber, GuessCount);
                 }
-                if (GuessedNumber == CorrectNumber)
+                if (GuessedNumber == CorrectAnswer)
                     CorrectGuess = true;
             }
 
             if (CorrectGuess)
             {
-                Console.WriteLine("You Answered correctly within '{0}' moves , the  number was '{1}'", GuessCount, CorrectNumber);
+                Console.WriteLine("You Answered correctly within '{0}' moves , the  number was '{1}'", GuessCount, CorrectAnswer);
                 Console.WriteLine("Press ESC to quit the game or press any other key to start again");
                 if (Console.ReadKey(true).Key == ConsoleKey.Escape)
                 {
--- GuessingApp/GuessingTests/GuessingTests.cs
+++ GuessingApp/GuessingTests/GuessingTests.cs
@@ -1,44 +1,58 @@
 using Xunit;
 using GuessingApp;
-
+using System;
+using System.IO;
 
 namespace GuessingTests
 {
     public class GuessingTests
     {
         [Fact]
-        public void RandomGivesCorrectNumberTest()
-        {
-            Assert.InRange(Game.GenerateRandomNumber(), 1, 300);
-        }
-        [Fact]
         public void RandomNumberIsIntegerTest()
         {
             Assert.IsType<int>(Game.GenerateRandomNumber());
         }
 
-        [Theory]
-        [InlineData(300)]
-        public void NumberControlReturnsNumberIfCorrectTest(int value)
+        [Fact]
+        public void RandomNumberIsValid()
         {
-            Assert.Equal(value, Game.IsNumberCorrect(300));
+            int rndNumber = Game.GenerateRandomNumber();
+
+            Assert.InRange(Game.GenerateRandomNumber(), 1, 300);
+            Assert.Equal(rndNumber, Game.IsNumberCorrect(rndNumber));
         }
 
         [Theory]
         [InlineData(1)]
         [InlineData(300)]
-        public void ValidNumberRangeTest(int value)
+        public void ValidUserInputReturnsNumberTest(int value)
         {
-            Assert.True(Game.IsValidRangeNumber(value));
+            string userInput = value.ToString();
+            var sr = new StringReader(userInput);
+            Console.SetIn(sr);
+
+            int parsed = Game.GetUserNumber();
+            Assert.InRange(parsed, 1, 300);
+            Assert.Equal(value, parsed);
         }
 
         [Theory]
-        [InlineData(500)]
-        [InlineData(0)]
-        [InlineData(-9)]
-        public void IncorrectNumberRangeTest(int value)
+        [InlineData(1)]
+        [InlineData(300)]
+        public void NumberCheckingWorksForValidTest(int value)
         {
-            Assert.False(Game.IsValidRangeNumber(value));
+            Assert.Equal(value, Game.IsNumberCorrect(value));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(3000)]
+        public void InvalidInputFixedByUserTest(int value)
+        {
+            var sr = new StringReader("250");
+            Console.SetIn(sr);
+
+            Assert.NotEqual(value, Game.IsNumberCorrect(value));
         }
     }
 }
